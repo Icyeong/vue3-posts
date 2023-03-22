6ee11358017b6c4ff82399eab82242e320b97a60
Imm Chaeyeong <dbcjswjs@gmail.com>
2023-03-22 15:10:17 +0900

    게시판 UI제작 및 라우터 적용

---
+++ src/api/posts.js
@@ -0,0 +1,11 @@
+const posts = [
+  { id: 1, title: '제목1', content: '내용1', createdAt: '2023-03-22' },
+  { id: 2, title: '제목2', content: '내용2', createdAt: '2023-03-23' },
+  { id: 3, title: '제목3', content: '내용3', createdAt: '2023-03-24' },
+  { id: 4, title: '제목4', content: '내용4', createdAt: '2023-03-25' },
+  { id: 5, title: '제목5', content: '내용5', createdAt: '2023-03-26' },
+];
+
+export function getPosts() {
+  return posts;
+}
