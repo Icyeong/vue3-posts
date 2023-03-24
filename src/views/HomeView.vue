b2c8f74c500c6f9bcdbd8ca0d3927b4046049f50
Imm Chaeyeong <dbcjswjs@gmail.com>
2023-03-24 11:23:31 +0900

    공통 컴포넌트 분리

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,26 @@
     <p>{{ $route.path }}</p>
     <p>{{ $route.name }}</p>
     <button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+    <hr class="my-4" />
+    <AppGrid v-slot="{ item }" :items="items">
+      <AppCard>{{ item }}</AppCard>
+    </AppGrid>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+
+import AppCard from '@/components/AppCard.vue';
+import AppGrid from '@/components/AppGrid.vue';
 
 const router = useRouter();
 const goAboutPage = () => {
   router.push('/about');
 };
+
+const items = ref(['사과', '포도', '딸기', '바나나']);
 </script>
 
 <style lang="scss" scoped></style>
