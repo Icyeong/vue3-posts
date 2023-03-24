53ef74ad336e3bf7aa372ad8bd29bb9e981517e4
Imm Chaeyeong <dbcjswjs@gmail.com>
2023-03-24 13:44:49 +0900

    Alert 공통 컴포넌트 제작

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -18,6 +18,7 @@
         <button class="btn btn-primary">수정</button>
       </template>
     </PostForm>
+    <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
   </div>
 </template>
 
@@ -26,6 +27,7 @@ import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getPostById, updatePost } from '@/api/posts';
 import PostForm from '@/components/posts/PostForm.vue';
+import AppAlert from '@/components/AppAlert.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -51,13 +53,28 @@ fetchPost();
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    router.push({ name: 'PostDetail', params: { id } });
+    // router.push({ name: 'PostDetail', params: { id } });
+    vAlert('수정이 완료되었습니다.', 'success');
   } catch (err) {
     console.error(err);
+    vAlert('네트워크 오류!');
   }
 };
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
+
+// alert
+const showAlert = ref(false);
+const alertMessage = ref('');
+const alertType = ref('');
+const vAlert = (message, type = 'error') => {
+  showAlert.value = true;
+  alertMessage.value = message;
+  alertType.value = type;
+  setTimeout(() => {
+    showAlert.value = false;
+  }, 2000);
+};
 </script>
 
 <style lang="scss" scoped></style>
