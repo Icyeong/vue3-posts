b4edefbabfae49680aea8fc421166457fb907c15
Imm Chaeyeong <dbcjswjs@gmail.com>
2023-03-24 14:44:06 +0900

    Transition & TransitionGroup 컴포넌트 제작

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -18,7 +18,8 @@
         <button class="btn btn-primary">수정</button>
       </template>
     </PostForm>
-    <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
+    <!-- <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" /> -->
+    <AppAlert :items="alerts" />
   </div>
 </template>
 
@@ -43,6 +44,7 @@ const fetchPost = async () => {
     setForm(data);
   } catch (err) {
     console.error(err);
+    vAlert(err.message);
   }
 };
 const setForm = ({ title, content }) => {
@@ -57,23 +59,26 @@ const edit = async () => {
     vAlert('수정이 완료되었습니다.', 'success');
   } catch (err) {
     console.error(err);
-    vAlert('네트워크 오류!');
+    vAlert(err.message);
   }
 };
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
 
 // alert
-const showAlert = ref(false);
-const alertMessage = ref('');
-const alertType = ref('');
+// const showAlert = ref(false);
+// const alertMessage = ref('');
+// const alertType = ref('');
+const alerts = ref([]);
 const vAlert = (message, type = 'error') => {
-  showAlert.value = true;
-  alertMessage.value = message;
-  alertType.value = type;
+  alerts.value.push({ message, type });
+  // showAlert.value = true;
+  // alertMessage.value = message;
+  // alertType.value = type;
   setTimeout(() => {
-    showAlert.value = false;
-  }, 2000);
+    // showAlert.value = false;
+    alerts.value.shift();
+  }, 1800);
 };
 </script>
 
