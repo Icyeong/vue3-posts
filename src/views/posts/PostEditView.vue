1b4e3ea1a17f6696303081aef47ea0fd14292117
Imm Chaeyeong <dbcjswjs@gmail.com>
2023-03-27 14:49:35 +0900

    alert 컴포저블 함수

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -18,8 +18,6 @@
         <button class="btn btn-primary">수정</button>
       </template>
     </PostForm>
-    <!-- <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" /> -->
-    <AppAlert :items="alerts" />
   </div>
 </template>
 
@@ -28,6 +26,9 @@ import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getPostById, updatePost } from '@/api/posts';
 import PostForm from '@/components/posts/PostForm.vue';
+import { useAlert } from '@/composables/alert';
+
+const { vAlert, vSuccess } = useAlert();
 
 const route = useRoute();
 const router = useRouter();
@@ -54,8 +55,8 @@ fetchPost();
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    // router.push({ name: 'PostDetail', params: { id } });
-    vAlert('수정이 완료되었습니다.', 'success');
+    router.push({ name: 'PostDetail', params: { id } });
+    vSuccess('수정이 완료되었습니다.');
   } catch (err) {
     console.error(err);
     vAlert(err.message);
@@ -63,22 +64,6 @@ const edit = async () => {
 };
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
-
-// alert
-// const showAlert = ref(false);
-// const alertMessage = ref('');
-// const alertType = ref('');
-const alerts = ref([]);
-const vAlert = (message, type = 'error') => {
-  alerts.value.push({ message, type });
-  // showAlert.value = true;
-  // alertMessage.value = message;
-  // alertType.value = type;
-  setTimeout(() => {
-    // showAlert.value = false;
-    alerts.value.shift();
-  }, 1800);
-};
 </script>
 
 <style lang="scss" scoped></style>
