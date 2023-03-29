67790ce3da8dde7a720000b8e4fae7ee65bed02e
Imm Chaeyeong <dbcjswjs@gmail.com>
2023-03-29 10:00:22 +0900

    개선사항 리팩토링

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">YEONG</a>
+        <RouterLink class="navbar-brand" to="/">YEONG</RouterLink>
         <button
           class="navbar-toggler"
           type="button"
